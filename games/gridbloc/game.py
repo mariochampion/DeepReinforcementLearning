d7642b2dc7c585fe23e720e42a025066585e0604
mariochampion <mario@mariochampion.com>
2018-03-19 14:50:26 -0500

    more game desc setup

--- games/gridbloc/game.py
+++ games/gridbloc/game.py
@@ -1,13 +1,15 @@
 '''
 PURPOSE
-the goal here is to mathematically describe the DUAL + OVERLAPPING grids of gridbloc (running tiles and blocking tiles)
-so that arrays of valid choices can be calculated. unlike most other board games, there are TWO rounds, and the winner of any one round is ALWAYS the runner.
- * The end-round condition is no valid-move un-scored tiles available for a runner.
+the goal here is to mathematically describe the DUAL + OVERLAPPING (or non-even row-count, if you like) grids of gridbloc game, which required both running tiles and blocking tiles, so that arrays of valid choices can be calculated as needed. 
+
+unlike most other board games, there are TWO rounds, and the winner of any one round is ALWAYS the runner, the winner of the game is whomever has the most running points after each player has run (so, 2 rounds in a 2 player game). runners earn points for each NEW tile they occupy during a game, and may return to already scored tiles as a matter of strategy, but wont get an extra point for that move. specifically, a tile can only be scored once per game.
+
+ * The end-round condition is no un-scored tiles available for a runner to reach with a valid move.
  * The end-game condition is the end of round two.
  * The winning condition is the higher run score AFTER end of round two.
- * "w" the parameter stands for the Width of a row or RUNNING tiles. NOTE: the blocking tiles are calculated from "w"
+ * "w" as a parameter below stands for the Width of a row or RUNNING tiles. NOTE: the blocking tiles are calculated from "w"
  * the upper left RUNNING tile is NEITHER 1 nor 0 (zero). the upper left column top tile is actually 1. a blocking tile ATOP each row uses numbers 1 to w. so then, w+1 is the left border wall for the first row, and the first running tile is w+2.
- * "current_tile" the parameter is the NUMBER of the tile, which is NOT a A7 or F2 chess style notation, but a simple integer calculated from row width, with additions for interjecting blocking tiles.
+ * "current_tile" as a parameter below is the NUMBER of the tile, which is NOT a A7 or F2 chess style notation, but a simple integer calculated from row width, according to the formulas which account for interposing blocking tiles.
 '''
 import numpy as np
 
