b8e823ee2d24a9f9bdd5f9175e44cdbb9db882ce
mariochampion <mario@mariochampion.com>
2018-03-19 16:33:36 -0500

    mo gametile formulas starting

--- games/gridbloc/game.py
+++ games/gridbloc/game.py
@@ -2,16 +2,39 @@
 PURPOSE
 the goal here is to mathematically describe the DUAL + OVERLAPPING (or non-even row-count, if you like) grids of gridbloc game, which required both running tiles and blocking tiles, so that arrays of valid choices can be calculated as needed. 
 
-unlike most other board games, there are TWO rounds, and the winner of any one round is ALWAYS the runner, the winner of the game is whomever has the most running points after each player has run (so, 2 rounds in a 2 player game). runners earn points for each NEW tile they occupy during a game, and may return to already scored tiles as a matter of strategy, but wont get an extra point for that move. specifically, a tile can only be scored once per game.
+unlike most other board games, there are TWO rounds, and the winner of the game is whomever has the most running points after each player has run (so, 2 rounds in a 2 player game). runners earn points for each NEW tile they occupy during a game, and may return to already scored tiles as a matter of strategy, but wont get an extra point for that move. specifically, a tile can only be scored once per game.
 
- * The end-round condition is no un-scored tiles available for a runner to reach with a valid move.
- * The end-game condition is the end of round two.
- * The winning condition is the higher run score AFTER end of round two.
- * "w" as a parameter below stands for the Width of a row or RUNNING tiles. NOTE: the blocking tiles are calculated from "w"
- * the upper left RUNNING tile is NEITHER 1 nor 0 (zero). the upper left column top tile is actually 1. a blocking tile ATOP each row uses numbers 1 to w. so then, w+1 is the left border wall for the first row, and the first running tile is w+2.
- * "current_tile" as a parameter below is the NUMBER of the tile, which is NOT a A7 or F2 chess style notation, but a simple integer calculated from row width, according to the formulas which account for interposing blocking tiles.
+IMPORTANT CONDITIONS
+ * runner moves first, blocker moves next. that is one turn.
+ * The end-round condition occurs when no un-scored tiles available for a runner to reach with a valid move.
+ * at the end of a round, the players swap roles. new runner makes first move.
+ * The end-game condition occurs at the end of round two.
+ * The winner has the higher run score AFTER end of round two.
+
+PARAMETERS 
+ * "h" = height of grid (number of rows)
+ * "w" =  Width of a row or RUNNING tiles. NOTE: the blocking tiles are calculated from "w"
+ * "n" = the row number (n=3 is the 3rd row, etc)
+ * "ct" = the NUMBER of the CurrentTile, which is NOT a A7 or F2 chess style notation, but a simple integer calculated from row width, according to the formulas which account for interposing blocking tiles.
+
+SOME FORMULAS
+ * run_row_length = 2w+1
+ * run_row_starter = nw + ( (n-1) + (2w+1) ) + 1
+ * run_row_tilerange = n + (2w-1) (by 2s)
+ * vert_tile = 3w+1 ## to current tile, add this value for below tile, and subtract for above tile.
+   ** vert_above = ct - vert_tile
+   ** vert_below = ct + vert_tile
+ * block_row_hor_starter = ( (n-1)(2w+1) ) + ( (n-1)(w)+1 )
+ * block_row_hor_range = Bn+1 in range(1,w)
+ * block_row_vert_starter = 
+ * block_row_vert_range = 
+
+NOTE: the upper left RUNNING tile is NEITHER 1 nor 0 (zero). the upper left column top tile is actually 1. a blocking tile ATOP each row uses numbers 1 to w. so then, w+1 is the left border wall for the first row, and the first running tile is w+2.
+
 '''
 import numpy as np
+
+
 
 
 ### util funcs to move at some point  
