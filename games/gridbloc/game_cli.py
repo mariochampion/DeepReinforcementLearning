45b3e9a5440641c6902f15b80ebe92cc290c1b02
air_mmc <mario@mariochampion.com>
2018-03-22 13:32:42 -0500

    adding CLI version for testing

---
+++ games/gridbloc/game_cli.py
@@ -0,0 +1,418 @@
+'''
+PURPOSE
+the goal here is to mathematically describe the DUAL + OVERLAPPING grids of gridbloc game, which required both running tiles and blocking tiles, so that arrays of valid choices can be calculated as needed. (the grid could, alternately, be said to have non-even row-count (ie, columns), if you like.) the grid for running tiles is pretty classic, just cap all 4 edges (in an rectangular grid version) of each running tile with a tile to block and you see the dual/overlapping/interposed grid system of gridbloc.
+
+unlike most other board games, there are TWO rounds, and the winner of the game is whomever has the most points after each player has run (so, 2 rounds in a 2 player game). runners earn points for each NEW tile they occupy during a game, and may return to already scored tiles as a matter of strategy, but wont get an extra point for that move. specifically, a tile can only be scored once per game, and blockers dont earn points, they just try to limit the points the runner earns by blocking them in.
+
+IMPORTANT CONDITIONS
+ * runner moves first, blocker moves next. that is one turn.
+ * The end-round condition occurs when no un-scored tiles available for a runner to reach with a valid move.
+ * at the end of a round, the players swap roles. new runner makes first move.
+ * The end-game condition occurs at the end of round two.
+ * The winner has the higher run score AFTER end of round two.
+
+
+DEV STEPS OF THIS FILE:
+1. how does this file relate to the building of arrays which are currently hardcoded into other example game.py files?
+2. probably i should just add my custom gridbloc array calculation functions to existing game.py and use function call outs rather than hardcoded arrays.
+3. CON: this might slow things down a bit as they will have to be re-calced, but they also might be so easy to calc that who cares
+4. alt to 3: calc them ahed of time and then include theoutputs as hardcoded, but include a game-setup.py style file for thsose interested, and for me (i m interested!)
+5. determine exact role of Game.gameState Game.actionSpace, GameState.winners (especially this last one as winner is simple the greater of p1.runscore and p2.runscore (or whatever that exact equivalent might be)
+6. currentBoard = state.board and currentAV = actionValues #investigate
+
+MVP CLI phases
+1. input w and h and it calcs gamestate and tile options arrays
+2. input ct param and it calcs the legal moves arrays for runner and blocker
+3. input ct and calcs GOOD moves not just legal moves for runner and blocker
+
+
+
+
+
+
+PARAMETERS 
+ * "h" = height of grid (number of rows)
+ * "w" =  width of a row in RUNNING tiles. 
+
+NOTE: blocking tiles (hor and vert, inclduing edge walls etc) will be calculated calculated from w & h 
+ * "n" is the row number (n=3 is the 3rd row, etc)
+ * "ct" is the NUMBER of the current_tile, which is NOT a A7 or F2 chess style notation, but a simple integer calculated from row width, according to the formulas which account for interposing blocking tiles.
+ * the upper left RUNNING tile is NEITHER 1 nor 0 (zero). the upper left column top tile is actually 1, as a blocking tile ATOP each row uses numbers 1 to w. so then, w+1 is the left border wall for the first row, and the first running tile is w+2.
+
+
+SOME FORMULAS
+# NOTE: need to calculate "ct" when starting, from the available runner options. then track as runner moves, using the formulas.
+# NOTE: "n" is calculated from ct and w, then track as runner moves, using the formulas.
+
+ * run_row_length = 2w+1
+ * n = floor( ct / run_row_length )
+ * run_row_starter = nw + ( (n-1) + (2w+1) ) + 1
+ * run_row_tilerange = range(run_row_starter, ( run_row_starter + (2w-2) ), 2) #step = 2
+
+ * vert_tile = 3w+1 ## to current tile, add this value for below tile, and subtract for above tile.
+   ** vert_above = ct - vert_tile
+   ** vert_below = ct + vert_tile
+
+ * block_row_hor_starter = ( (n-1)(2w+1) ) + ( (n-1)(w)+1 )
+ * block_row_hor_range = b+1 in range(1,w) # NOT COMPLETE
+ * block_row_vert_starter = run_row_starter -1
+ * block_row_vert_ender = block_row_vert_starter + 2w
+ * block_row_vert_range = range(block_row_vert_starter: block_row_vert_ender, 2) #w/step = 2
+ 
+ * ct_available_start = []
+ * ct_available_legal = []
+ 
+ # close_edges() is the function to identify all the edge tiles to prevent PACMAN style movement. (for now!)
+'''
+
+
+import sys
+import numpy as np
+import logging
+import math
+
+
+### util funcs to move at some point  
+### ---------------------------------------
+
+def row_running_len(w):
+  '''
+  for every tile, there is a left side, then one more right side at the end
+  (and of course you can think of this as rights with one left)
+  '''
+  lenrow = 2 * w + 1
+  return lenrow
+  
+  
+def row_blocking_len(w):
+  '''
+  a row of top or bottom edges, (between runnable tiles) is simply as long as w
+  '''
+  return w
+  
+
+def tile_up(w, ct):
+  '''
+  a running or blocking tile is 3*w+1 from the current_tile
+  '''  
+  uptile = ct - (3*w) + 1
+  return uptile
+  
+def tile_down(w, ct):
+  '''
+  a running or blocking tile is 3*w+1 from the current_tile
+  '''  
+  downtile = ct + (3*w) + 1
+  return downtile  
+
+### ---------------------------------------
+
+
+
+
+def build_gameboard(w,h):
+  '''
+  do a series of functions:
+  1. build mastergrid
+  2. build wall masterlist
+    (sublists: edges, used, nextmoveoptions, best)
+  3. build tile masterlist
+    (sublists: rowcaps, used, nextmoveoptions, best)
+  '''
+def build_mastergrid(w,h):
+  '''
+  w = width
+  h = height
+  formula involves adding walls and edges, so not just w * h
+  return list of tiles in one long array as well as arrays for tiles, walls
+  '''
+  
+  
+  return mastergrid
+  
+  
+  
+  
+
+
+def tiles_valid(ct):
+  '''
+  return a LIST of valid tiles, NOT SORTED BY BEST. just legal tiles.
+  another func will sort them by best. 
+  '''
+  
+  tiles_valid = [1,2,3]
+  
+  return tiles_valid
+  
+
+
+  
+def blocks_valid():
+  '''
+  returns LIST of valid blocks, NOT SORTED BY BEST blocks
+  another func will sort them by best.
+  (will this not be ALL walls/blocks MINUS already walls?)
+  i guess then: call the get_blocks_blocked
+  '''  
+  
+  blocks_valid = [1,2,3,4,5,6]
+  return blocks_valid
+  
+
+
+
+################################
+################################ original file for connect4
+
+class Game:
+
+	def __init__(self):		
+		self.currentPlayer = 1
+		self.gameState = GameState(np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], dtype=np.int), 1)
+		self.actionSpace = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], dtype=np.int)
+		self.pieces = {'1':'X', '0': '-', '-1':'O'}
+		self.grid_shape = (6,7)
+		self.input_shape = (2,6,7)
+		self.name = 'connect4'
+		self.state_size = len(self.gameState.binary)
+		self.action_size = len(self.actionSpace)
+
+	def reset(self):
+		self.gameState = GameState(np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], dtype=np.int), 1)
+		self.currentPlayer = 1
+		return self.gameState
+
+	def step(self, action):
+		next_state, value, done = self.gameState.takeAction(action)
+		self.gameState = next_state
+		self.currentPlayer = -self.currentPlayer
+		info = None
+		return ((next_state, value, done, info))
+
+	def identities(self, state, actionValues):
+		identities = [(state,actionValues)]
+
+		currentBoard = state.board
+		currentAV = actionValues
+
+		currentBoard = np.array([
+			  currentBoard[6], currentBoard[5],currentBoard[4], currentBoard[3], currentBoard[2], currentBoard[1], currentBoard[0]
+			, currentBoard[13], currentBoard[12],currentBoard[11], currentBoard[10], currentBoard[9], currentBoard[8], currentBoard[7]
+			, currentBoard[20], currentBoard[19],currentBoard[18], currentBoard[17], currentBoard[16], currentBoard[15], currentBoard[14]
+			, currentBoard[27], currentBoard[26],currentBoard[25], currentBoard[24], currentBoard[23], currentBoard[22], currentBoard[21]
+			, currentBoard[34], currentBoard[33],currentBoard[32], currentBoard[31], currentBoard[30], currentBoard[29], currentBoard[28]
+			, currentBoard[41], currentBoard[40],currentBoard[39], currentBoard[38], currentBoard[37], currentBoard[36], currentBoard[35]
+			])
+
+		currentAV = np.array([
+			currentAV[6], currentAV[5],currentAV[4], currentAV[3], currentAV[2], currentAV[1], currentAV[0]
+			, currentAV[13], currentAV[12],currentAV[11], currentAV[10], currentAV[9], currentAV[8], currentAV[7]
+			, currentAV[20], currentAV[19],currentAV[18], currentAV[17], currentAV[16], currentAV[15], currentAV[14]
+			, currentAV[27], currentAV[26],currentAV[25], currentAV[24], currentAV[23], currentAV[22], currentAV[21]
+			, currentAV[34], currentAV[33],currentAV[32], currentAV[31], currentAV[30], currentAV[29], currentAV[28]
+			, currentAV[41], currentAV[40],currentAV[39], currentAV[38], currentAV[37], currentAV[36], currentAV[35]
+					])
+
+		identities.append((GameState(currentBoard, state.playerTurn), currentAV))
+
+		return identities
+
+
+class GameState():
+	def __init__(self, board, playerTurn):
+		self.board = board
+		self.pieces = {'1':'X', '0': '-', '-1':'O'}
+		self.winners = [
+			[0,1,2,3],
+			[1,2,3,4],
+			[2,3,4,5],
+			[3,4,5,6],
+			[7,8,9,10],
+			[8,9,10,11],
+			[9,10,11,12],
+			[10,11,12,13],
+			[14,15,16,17],
+			[15,16,17,18],
+			[16,17,18,19],
+			[17,18,19,20],
+			[21,22,23,24],
+			[22,23,24,25],
+			[23,24,25,26],
+			[24,25,26,27],
+			[28,29,30,31],
+			[29,30,31,32],
+			[30,31,32,33],
+			[31,32,33,34],
+			[35,36,37,38],
+			[36,37,38,39],
+			[37,38,39,40],
+			[38,39,40,41],
+
+			[0,7,14,21],
+			[7,14,21,28],
+			[14,21,28,35],
+			[1,8,15,22],
+			[8,15,22,29],
+			[15,22,29,36],
+			[2,9,16,23],
+			[9,16,23,30],
+			[16,23,30,37],
+			[3,10,17,24],
+			[10,17,24,31],
+			[17,24,31,38],
+			[4,11,18,25],
+			[11,18,25,32],
+			[18,25,32,39],
+			[5,12,19,26],
+			[12,19,26,33],
+			[19,26,33,40],
+			[6,13,20,27],
+			[13,20,27,34],
+			[20,27,34,41],
+
+			[3,9,15,21],
+			[4,10,16,22],
+			[10,16,22,28],
+			[5,11,17,23],
+			[11,17,23,29],
+			[17,23,29,35],
+			[6,12,18,24],
+			[12,18,24,30],
+			[18,24,30,36],
+			[13,19,25,31],
+			[19,25,31,37],
+			[20,26,32,38],
+
+			[3,11,19,27],
+			[2,10,18,26],
+			[10,18,26,34],
+			[1,9,17,25],
+			[9,17,25,33],
+			[17,25,33,41],
+			[0,8,16,24],
+			[8,16,24,32],
+			[16,24,32,40],
+			[7,15,23,31],
+			[15,23,31,39],
+			[14,22,30,38],
+			]
+		self.playerTurn = playerTurn
+		self.binary = self._binary()
+		self.id = self._convertStateToId()
+		self.allowedActions = self._allowedActions()
+		self.isEndGame = self._checkForEndGame()
+		self.value = self._getValue()
+		self.score = self._getScore()
+
+	def _allowedActions(self):
+		allowed = []
+		for i in xrange(len(self.board)):
+			if i >= len(self.board) - 7:
+				if self.board[i]==0:
+					allowed.append(i)
+			else:
+				if self.board[i] == 0 and self.board[i+7] != 0:
+					allowed.append(i)
+
+		return allowed
+
+	def _binary(self):
+
+		currentplayer_position = np.zeros(len(self.board), dtype=np.int)
+		currentplayer_position[self.board==self.playerTurn] = 1
+
+		other_position = np.zeros(len(self.board), dtype=np.int)
+		other_position[self.board==-self.playerTurn] = 1
+
+		position = np.append(currentplayer_position,other_position)
+
+		return (position)
+
+	def _convertStateToId(self):
+		player1_position = np.zeros(len(self.board), dtype=np.int)
+		player1_position[self.board==1] = 1
+
+		other_position = np.zeros(len(self.board), dtype=np.int)
+		other_position[self.board==-1] = 1
+
+		position = np.append(player1_position,other_position)
+
+		id = ''.join(map(str,position))
+
+		return id
+
+	def _checkForEndGame(self):
+		if np.count_nonzero(self.board) == 42:
+			return 1
+
+		for x,y,z,a in self.winners:
+			if (self.board[x] + self.board[y] + self.board[z] + self.board[a] == 4 * -self.playerTurn):
+				return 1
+		return 0
+
+
+	def _getValue(self):
+		# This is the value of the state for the current player
+		# i.e. if the previous player played a winning move, you lose
+		for x,y,z,a in self.winners:
+			if (self.board[x] + self.board[y] + self.board[z] + self.board[a] == 4 * -self.playerTurn):
+				return (-1, -1, 1)
+		return (0, 0, 0)
+
+
+	def _getScore(self):
+		tmp = self.value
+		return (tmp[1], tmp[2])
+
+
+
+
+	def takeAction(self, action):
+		newBoard = np.array(self.board)
+		newBoard[action]=self.playerTurn
+		
+		newState = GameState(newBoard, -self.playerTurn)
+
+		value = 0
+		done = 0
+
+		if newState.isEndGame:
+			value = newState.value[0]
+			done = 1
+
+		return (newState, value, done) 
+
+
+
+
+	def render(self, logger):
+		for r in range(6):
+			logger.info([self.pieces[str(x)] for x in self.board[7*r : (7*r + 7)]])
+		logger.info('--------------')
+
+
+
+def main(args):
+  print "in main"
+  print
+  for arg in args:
+    print arg
+  
+  
+
+
+  
+  #################################
+# boilerplate kicker offer (yes thats a tech term!)   
+if __name__ == '__main__':
+  
+  print "GRIDBLOC SETUP CLI VERSION"
+  print
+  
+  try:
+    args
+  except:
+    args = sys.argv[1:]
+  
+  main(args)
