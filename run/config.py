10f6ae6b2d649f8a1e30d6ae267684920fd8a1bb
mariochampion <mario@mariochampion.com>
2018-04-02 10:50:56 -0500

    tweak some training vars

--- run/config.py
+++ run/config.py
@@ -1,9 +1,9 @@
 
 
 #### SELF PLAY
-EPISODES = 30
+EPISODES = 20
 MCTS_SIMS = 50
-MEMORY_SIZE = 30000
+MEMORY_SIZE = 24000
 TURNS_UNTIL_TAU0 = 10 # turn on which it starts playing deterministically
 CPUCT = 1
 EPSILON = 0.2
